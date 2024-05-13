90f7ab3119df317c8fdce237cf6e578dd6e9d333
abhishek18124 <abhishek18124@iiitd.ac.in>
2024-05-13 18:54:51 +0530

    Lecture 07

---
+++ Lecture 07/015CountSetBitsOptimised.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	int n = 42;
+	int cnt = 0; // to track the no. of set bits in 'n'
+
+	// todo ...
+
+	return 0;
+}
---
+++ Lecture 07/017CheckPowerOf2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	int n = 8;
+
+	// todo ...
+
+	return 0;
+}
