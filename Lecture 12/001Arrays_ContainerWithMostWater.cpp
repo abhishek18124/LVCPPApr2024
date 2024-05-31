5e92f5c9d6dcd5cb85704787160bc4b963fd2dd5
abhishek18124 <abhishek18124@iiitd.ac.in>
2024-05-31 18:40:43 +0530

    Lecture 12

---
+++ Lecture 12/001Arrays_ContainerWithMostWater.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main() {	
+
+	int height[] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+	int n = sizeof(height)/sizeof(int);
+
+	return 0;
+}
---
+++ Lecture 12/002Arrays_MergeSortedArrays.cpp
@@ -0,0 +1,21 @@
+// contraints : m <= 10, n <= 10
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	int arr[] = {10, 30, 50, 70};
+	int brr[] = {20, 40, 60};
+
+	int n = sizeof(arr) / sizeof(int);
+	int m = sizeof(brr) / sizeof(int);
+
+	int crr[20];
+
+	// todo ...
+
+	return 0;
+}
+
