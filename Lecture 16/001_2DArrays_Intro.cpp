62710759189838728b56c9fd165cd3973cd50c37
abhishek18124 <abhishek18124@iiitd.ac.in>
2024-06-17 19:18:34 +0530

    Lecture 16

---
+++ Lecture 16/001_2DArrays_Intro.cpp
@@ -0,0 +1,37 @@
+/*
+
+	Constraints
+
+	0 < m, n <= 10
+
+	here, m denotes the no. of rows in the given 2D array
+	      n denotes the no. of cols in the given 2D array
+
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	int arr[10][10];
+
+	int m;
+	cin >> m;
+
+	int n;
+	cin >> n;
+
+	// read mxn values into the 2D array
+
+	// todo ...
+
+	// print mxn values of the 2D array
+
+	// todo ...
+
+	return 0;
+}
+
+
