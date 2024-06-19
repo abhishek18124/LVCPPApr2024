42af25f5f5d4065f8a84a4b37e33e7cf6aef2d15
abhishek18124 <abhishek18124@iiitd.ac.in>
2024-06-19 18:43:30 +0530

    Lecture 17

---
+++ Lecture 17/005DMA_DynamicArray.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+using namespace std;
+
+resize(int* A, int c) {
+	cout << "resizing from "
+	     << c*sizeof(int) << "B to "
+	     << 2 * c*sizeof(int) << "B" << endl;
+	// todo ...
+}
+
+int main() {
+
+	int* A = new int[1];
+	int c = 1; // to store the maximum capacity of the dynamic array
+	int i = 0; // to store the size of the dynamic array
+
+	int x; // to store the integer value read from the user
+
+	while (true) {
+
+		cin >> x;
+
+		// todo ...
+
+	}
+
+	// print the dynamic array
+
+	for (int j = 0; j < i; j++) {
+		cout << A[j] << " ";
+	}
+
+	cout << endl;
+
+	return 0;
+
+}
---
+++ Lecture 17/006DMA_2DArrayOnHeaps.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	int m;
+	cin >> m;
+
+	int n;
+	cin >> n;
+
+	// create a 2D array of dimension 'mxn' on the heap memory
+
+	// read values into the 2D array
+
+	// print value of the 2D array
+
+	// deallocate memory for 2D array
+
+	return 0;
+}
