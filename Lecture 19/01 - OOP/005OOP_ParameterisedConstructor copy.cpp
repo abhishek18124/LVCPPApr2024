1995d938dc779bb29cbe942d6b3138c68e7d9bf0
abhishek18124 <abhishek18124@iiitd.ac.in>
2024-06-24 18:54:47 +0530

    Lecture 19

---
+++ Lecture 19/01 - OOP/005OOP_ParameterisedConstructor copy.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+class customer {
+
+public :
+
+	string name;
+	int age;
+	char gender;
+	double credits;
+
+	void print() {
+		cout << "name = " << name << endl;
+		cout << "age = " << age << endl;
+		cout << "gender = " << gender << endl;
+		cout << "credits = " << credits << endl;
+	}
+};
+
+
+
+int main() {
+
+
+	return 0;
+}
---
+++ Lecture 19/01 - OOP/021OOP_DynamicObjects.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+
+class customer {
+
+public :
+
+	string name;
+	int age;
+	char gender;
+	double credits;
+
+	customer() {
+		cout << "\ninside the default constructor of the \'customer\' class\n" << endl;
+	}
+
+	customer(string n, int a, char g, double c) {
+		cout << "\ninside the parameterised constructor of the \'customer\' class\n" << endl;
+		name = n;
+		age = a;
+		gender = g;
+		credits = c;
+	}
+
+	void print() {
+		cout << "name = " << name << endl;
+		cout << "age = " << age << endl;
+		cout << "gender = " << gender << endl;
+		cout << "credits = " << credits << endl << endl;
+	}
+
+};
+
+int main() {
+
+
+	return 0;
+}
