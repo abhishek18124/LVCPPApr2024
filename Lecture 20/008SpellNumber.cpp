bc29cfe8b9917f96e56d6c74260ff66feecc80e8
abhishek18124 <abhishek18124@iiitd.ac.in>
2024-07-01 18:52:34 +0530

    Lecture 20

---
+++ Lecture 20/008SpellNumber.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+string spellingMap[] = { "zero", "one", "two", "three",
+                         "four", "five", "six", "seven",
+                       	 "eight", "nine"};
+ 
+int main() {
+
+	int n = 123;
+
+	return 0;
+}
---
+++ Lecture 20/009StringToInteger.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	string str = "123";
+	int n = str.size();
+
+	return 0;
+}
