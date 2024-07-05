5d94def39de3ec01fe876a1291edb171fca87dd2
abhishek18124 <abhishek18124@iiitd.ac.in>
2024-07-05 19:45:46 +0530

    Lecture 21 - II

---
+++ Lecture 21 - II/011InversionCount.cpp
@@ -0,0 +1,15 @@
+// constraints : n <= 100
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	int arr[] = {2, 7, 5, 8, 3, 4};
+	int n = sizeof(arr) / sizeof(int);
+
+	// todo ...
+
+	return 0;
+}
