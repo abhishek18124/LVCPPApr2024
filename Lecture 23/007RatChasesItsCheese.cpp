22c614238356ce2f5a798a2625e52299a2439184
abhishek18124 <abhishek18124@iiitd.ac.in>
2024-07-15 18:03:38 +0530

    Lecture 23

---
+++ Lecture 23/007RatChasesItsCheese.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	char maze[][10] = {
+		"0X00",
+		"00X0",
+		"000X",
+		"0X00"
+	};
+
+	char path[][10] = {
+		"0000",
+		"0000",
+		"0000",
+		"0000"
+	};
+
+	int m = 4;
+	int n = 4;
+
+	// todo ...
+
+	return 0;
+}
