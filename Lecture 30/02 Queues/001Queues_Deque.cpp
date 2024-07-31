0a7b6d64a4e86a31c54d81fb341d88e0fd409ed7
abhishek18124 <abhishek18124@iiitd.ac.in>
2024-07-31 18:14:31 +0530

    Lecture 30

---
+++ Lecture 30/02 Queues/001Queues_Deque.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<deque>
+
+using namespace std;
+
+template <typename T>
+class queue {
+
+	deque<T> d;
+
+public:
+
+	void push(T val) {
+
+	}
+
+	void pop() {
+
+	}
+
+	T front() {
+
+	}
+
+	int size() {
+
+	}
+
+	bool empty() {
+
+	}
+
+};
+
+int main() {
+
+	queue<int> q;
+
+	cout << "size : " << q.size() << endl;
+	cout << "empty : " << q.empty() << endl;
+
+	q.push(10);
+	q.push(20);
+	q.push(30);
+
+	cout << "size : " << q.size() << endl;
+	cout << "front : " << q.front() << endl;
+
+	q.pop();
+
+	cout << "size : " << q.size() << endl;
+	cout << "front : " << q.front() << endl;
+
+	q.pop();
+
+	cout << "size : " << q.size() << endl;
+	cout << "front : " << q.front() << endl;
+
+	q.pop();
+	q.empty() ? cout << "queue is empty!" << endl : cout << "queue is not empty!" << endl;
+
+	return 0;
+}
---
+++ Lecture 30/02 Queues/003Queues_STL.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+int main() {
+
+	queue<int> q;
+
+	cout << "size : " << q.size() << endl;
+	cout << "empty : " << q.empty() << endl;
+
+	q.push(10);
+	q.push(20);
+	q.push(30);
+
+	cout << "size : " << q.size() << endl;
+	cout << "front : " << q.front() << endl;
+
+	q.pop();
+
+	cout << "size : " << q.size() << endl;
+	cout << "front : " << q.front() << endl;
+
+	q.pop();
+
+	cout << "size : " << q.size() << endl;
+	cout << "front : " << q.front() << endl;
+
+	q.pop();
+	q.empty() ? cout << "queue is empty!" << endl : cout << "queue is not empty!" << endl;
+
+	return 0;
+}
