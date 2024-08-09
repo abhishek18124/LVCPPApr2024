f14e3a3b738c94fb37ba9ac0e76794f4e4fe8f80
abhishek18124 <abhishek18124@iiitd.ac.in>
2024-08-09 18:49:00 +0530

    Lecture 32

---
+++ Lecture 32/002BinaryTrees_PathSumII.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class TreeNode {
+
+public :
+
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+
+	TreeNode(int val) {
+		this->val = val;
+		this->left = NULL;
+		this->right = NULL;
+	}
+
+};
+
+
+int main() {
+
+	TreeNode* root = NULL;
+
+	root = new TreeNode(5);
+
+	root->left = new TreeNode(4);
+	root->left->left = new TreeNode(11);
+	root->left->left->left = new TreeNode(7);
+	root->left->left->right = new TreeNode(2);
+
+	root->right = new TreeNode(8);
+	root->right->left = new TreeNode(13);
+	root->right->right = new TreeNode(4);
+	root->right->right->right = new TreeNode(1);
+
+	int targetSum = 22;
+
+	// todo ...
+
+	return 0;
+}
