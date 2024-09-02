766d71a02f1108d3e179ca8cc56dbe2ecf0ce701
abhishek18124 <abhishek18124@iiitd.ac.in>
2024-09-02 21:48:52 +0530

    Lecture 40

--- Lecture 40/001DP_FibonacciNumbers.cpp
+++ Lecture 40/001DP_FibonacciNumbers.cpp
@@ -13,7 +13,11 @@ Example :
 
 using namespace std;
 
+int cnt1 = 0;
+
 int f(int n) {
+
+	cnt1++;
 
 	// base case
 
@@ -34,11 +38,107 @@ int f(int n) {
 	return f(n - 1) + f(n - 2);
 }
 
+int cnt2 = 0;
+
+int fTopDown(int n, vector<int>& dp) {
+
+	cnt2++;
+
+	// lookup
+
+	if (dp[n] != -1) {
+
+		// you've previously solved f(n)
+
+		return dp[n];
+
+	}
+
+	// base case
+
+	if (n == 0 || n == 1) {
+
+		return dp[n] = n;
+
+	}
+
+	// recursive case
+
+	return dp[n] = fTopDown(n - 1, dp) + fTopDown(n - 2, dp);
+
+}
+
+// time : n+1.const ~ O(n)
+// space: n+1 due to dp[] ~ O(n)
+
+int fBottomUp(int n) {
+
+	vector<int> dp(n + 1);
+
+	dp[0] = 0; // at the 0th index of dp[] we store f(0)
+	dp[1] = 1; // at the 1st index of dp[] we store f(1)
+
+	for (int i = 2; i <= n; i++) {
+		// dp[i] = f(i) = ith fib. no.
+		dp[i] = dp[i - 1] + dp[i - 2]; // const
+	}
+
+	for (int i = 0; i < dp.size(); i++) {
+		cout << dp[i] << " ";
+	}
+
+	cout << endl;
+
+	return dp[n]; // at the nth index of dp[] we store f(n)
+}
+
+// time : O(n)
+// space: O(1)
+
+int fBottomUpSpaceOptimised(int n) {
+
+	if (n == 0 || n == 1) {
+		return n;
+	}
+
+	int a = 0; // 0th fib. no.
+	int b = 1; // 1st fib. no.
+
+	for (int i = 2; i <= n; i++) {
+
+		int c = a + b;
+		a = b;
+		b = c;
+
+	}
+
+	return b; // b represents the nth fib. no.
+
+}
+
 int main() {
 
-	int n = 7;
+	int n = 20;
 
 	cout << f(n) << endl;
+
+	cout << "cnt1 = " << cnt1 << endl;
+
+	vector<int> dp(n + 1, -1);
+
+	cout << fTopDown(n, dp) << endl;
+
+	cout << "cnt2 = " << cnt2 << endl;
+
+	for (int i = 0; i < dp.size(); i++) {
+		cout << dp[i] << " ";
+	}
+
+	cout << endl;
+
+	cout << fBottomUp(n) << endl;
+
+	cout << fBottomUpSpaceOptimised(n) << endl;
 
 	return 0;
 }
--- Lecture 40/002DP_MinStepsToOne.cpp
+++ Lecture 40/002DP_MinStepsToOne.cpp
@@ -18,10 +18,15 @@ Example :
 
 #include<iostream>
 #include<vector>
+#include<climits>
 
 using namespace std;
 
+int cnt1 = 0;
+
 int f(int n) {
+
+	cnt1++;
 
 	// base case
 
@@ -37,15 +42,143 @@ int f(int n) {
 
 	// f(n) = count min. no. of steps requried to reduce n to 1
 
-	// todo ...
+	// decide the next step
+
+	// option 1 : reduce n to n - 1
+
+	int x = f(n - 1);
+
+	// option 2 : reduce n to n / 2 only if n%2 == 0
+
+	int y = INT_MAX;
+
+	if (n % 2 == 0) {
+		y = f(n / 2);
+	}
+
+	// option 3 : reduce n to n / 3 only if n%3 == 0
+
+	int z = INT_MAX;
+
+	if (n % 3 == 0) {
+		z = f(n / 3);
+	}
+
+	return 1 + min(x, min(y, z));
+
+}
+
+int cnt2 = 0;
+
+int fTopDown(int n, vector<int>& dp) {
+
+	cnt2++;
+
+	// lookup
+
+	if (dp[n] != -1) {
+
+		// you alredy know min. steps required to reduce n to 1
+		return dp[n];
+
+	}
+
+	// base case
+
+	if (n == 1) {
+
+		// f(1) = count min. no. of steps required to reduce 1 to 1
+
+		return dp[n] = 0;
+
+	}
+
+	// recursive case
+
+	// f(n) = count min. no. of steps requried to reduce n to 1
+
+	// decide the next step
+
+	// option 1 : reduce n to n - 1
+
+	int x = fTopDown(n - 1, dp);
+
+	// option 2 : reduce n to n / 2 only if n%2 == 0
+
+	int y = INT_MAX;
+
+	if (n % 2 == 0) {
+		y = fTopDown(n / 2, dp);
+	}
+
+	// option 3 : reduce n to n / 3 only if n%3 == 0
+
+	int z = INT_MAX;
+
+	if (n % 3 == 0) {
+		z = fTopDown(n / 3, dp);
+	}
+
+	return dp[n] = 1 + min(x, min(y, z));
+
+}
+
+// time : O(n)
+// space: O(n) due to dp[]
+
+int fBottomUp(int n) {
+
+	vector<int> dp(n + 1); // 0th index is not used
+
+	dp[1] = 0; // at the 1st index of dp[] we store f(1)
+
+	for (int i = 2; i <= n; i++) {
+
+		// dp[i] = f(i) = min. steps required to reduce i to 1
+
+		// decide the next step
+
+		// option 1 : reduce i to i - 1
+
+		int x = dp[i - 1];
+
+		// option 2 : reduce i to i / 2
+
+		int y = INT_MAX;
+		if (i % 2 == 0) {
+			y = dp[i / 2];
+		}
+
+		// option 3 : reduce i to i / 3
+
+		int z = INT_MAX;
+		if (i % 3 == 0) {
+			z = dp[i / 3];
+		}
+
+		dp[i] = 1 + min(x, min(y, z)); // dp[i] = 1 + min(dp[i-1], min(dp[i/2], dp[i/3]))
+
+	}
+
+	return dp[n]; // at the nth index of dp[] we store f(n)
 
 }
 
 int main() {
 
-	int n = 10;
+	int n = 20;
 
 	cout << f(n) << endl;
+
+	cout << "cnt1 = " << cnt1 << endl;
+
+	vector<int> dp(n + 1, -1); // 0th index is not used
+
+	cout << fTopDown(n, dp) << endl;
+
+	cout << "cnt2 = " << cnt2 << endl;
+
+	cout << fBottomUp(n) << endl;
 
 	return 0;
 }
